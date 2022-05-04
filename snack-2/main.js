7a6268bddb117e7cbe389c1ba13624436707ba05
olic96 <francoolich@gmail.com>
2022-05-04 11:48:14 +0200

    snack 2

---
+++ snack-2/main.js
@@ -0,0 +1,7 @@
+const wordOne = prompt ("Inserisci la prima parola");
+const wordTwo = prompt ("Inserisci la seconda parola");
+
+if(wordOne.length < wordTwo.length) {
+    console.log(wordOne, wordTwo);
+    
+} 
