024b9707eb0cf4e1c4e6f797d624cc9de8342aa9
olic96 <francoolich@gmail.com>
2022-05-04 12:11:32 +0200

    snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,6 @@
+// chiedo 10 volte all'utente di inserire un numero,
+const aNumber = Number(prompt("Type a number"));
+for (i = 1, i < 10, i++) {
+    console.log(aNumber);
+}
+// stampa la somma di tutti i numeri inseriti.
