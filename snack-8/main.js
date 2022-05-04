d0f5b35f9c6a8886aeae3c45d35481703348ff0b
olic96 <francoolich@gmail.com>
2022-05-04 18:41:07 +0200

    snack 8

---
+++ snack-8/main.js
@@ -0,0 +1,10 @@
+// creo un array di numeri interi
+const listNumber = [1, 2, 3, 4, 5];
+// fai la somma di tutti gli elementi solo in posizione dispari
+
+for(let i = 0; i < listNumber.length; i++){
+    if(i % 2 === 1);
+    const somma = listNumber[i];
+}
+
+console.log(somma);
