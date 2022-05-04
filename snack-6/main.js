225f0a2459abfa1fb5119034cda60090e324a8a9
olic96 <francoolich@gmail.com>
2022-05-04 16:41:25 +0200

    correzione snack 6

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,11 +1,11 @@
 // chiedo all'utente di inserire un numero
-const userNumber = Number(prompt = ("Inserisci un numero"));
+let userNumber = Number(prompt("Inserisci un numero"));
 
 // SE è pari stampo il numero
 // ALTRIMENTI stampo il successivo 
 if(userNumber % 2 === 0) {
     console.log(userNumber);
-} else(userNumber % 2 === 1) {
-    let nextNumber = (userNumber + 1);
-    console.log(nextNumber);
+} else {
+    userNumber = userNumber + 1;
+    console.log(userNumber);
 }
