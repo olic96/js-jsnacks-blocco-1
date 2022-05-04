ba89faef8ff1326281335a8a5d128a12050df8e1
olic96 <francoolich@gmail.com>
2022-05-04 18:41:46 +0200

    correzione snack 7

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,12 +1,14 @@
-// creo un lista vuota
-const nominativi = [];
-// creo una lista di nomi 
-const nomi = ["Pino", "Gino", "Lino"];
-// creo una lista di cognomi
-const cognomi = ["Salame", "Prosciutto", "Mortadella"];
-// creo dei nominativi con i nomi e i cognomi delle due liste
-for (let i= 0; i < nominativi; i++) {
-    const nominativo = nomi[i] + cognomi[i];
-    nominativi.push (nominativo);
+// creo una lista vuota
+const listaNomi = [];
+// lista nomi
+const nomi = ["Gino", "Pino", "Lino"];
+// lista cognomi
+const cognomi = ["Rossi", "Verdi", "Bianchi"]
+
+// console.log(listaNomi.length);
+
+for (let i = 0; i < 3; i++) {
+   const randomPositionNomi =  Math.floor(Math.random() * 3);
+   const randomPositionCognomi =  Math.floor(Math.random() * 3);
+   console.log(nomi[randomPositionNomi], cognomi [randomPositionCognomi]);
 }
-console.log(nominativi);
