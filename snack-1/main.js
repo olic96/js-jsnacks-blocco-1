49512bb6b5aaa23e65873532ad0129b25dae98c7
olic96 <francoolich@gmail.com>
2022-05-04 11:36:25 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,8 @@
+const numberOne = prompt (Number("Inserisci il primo numero"));
+const numberTwo = prompt (Number("Inserisci il secondo numero"));
+
+if(numberOne > numberTwo) {
+    console.log(numberOne);
+} else {
+    console.log(numberTwo);
+}
